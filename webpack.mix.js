86297966b362928b5375beb83256e76ee183c9c2
Leandro Moreno <123857@gmail.com>
2021-10-25 20:36:24 -0500

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,11 +10,14 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
+mix.babelConfig({
+    plugins: ['@babel/plugin-syntax-dynamic-import'],
+});
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/material-dashboard.scss', 'public/material/css')
     .browserSync('funes-laravel.test')
     .version()
+    .sourceMaps()
     .options({
         hmrOptions: {
             host: 'localhost',
@@ -22,9 +25,40 @@ mix.js('resources/js/app.js', 'public/js')
         },
     })
     .webpackConfig({
-      devServer: {
-          port: '9000',
-          compress: 'true',
-          http2: 'true'
-      },
+        module: {
+            rules: [
+                {
+                    test: /\.jsx?$/,
+                    exclude: /node_modules(?!\/foundation-sites)|bower_components/,
+                    use: [
+                        {
+                            loader: 'babel-loader',
+                            options: Config.babel()
+                        }
+                    ]
+                }
+            ]
+        },
+        resolve: {
+            alias: {
+                '@': path.resolve('resources/assets/sass')
+            }
+        }
+    })
+    .webpackConfig({
+        devServer: {
+            port: '9000',
+            compress: 'true',
+            http2: 'true'
+        },
     });
+mix.options({
+    extractVueStyles: true,
+    processCssUrls: false
+});
+mix.webpackConfig({
+    output: {
+        publicPath: '/',
+        chunkFilename: 'js/[name].[chunkhash].js',
+    }
+});
