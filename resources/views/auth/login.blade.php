5b31457d8413592c8f32888402935772be4111d9
Leandro Moreno <123857@gmail.com>
2021-11-06 16:49:03 -0500

    Adjustement for auth styles

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,80 +1,91 @@
 @extends('layouts.app', ['class' => 'off-canvas-sidebar', 'activePage' => 'login', 'title' => __('Material Dashboard')])
 
 @section('content')
-<div class="container" style="height: auto;">
-  <div class="row align-items-center">
-    <div class="col-md-9 ml-auto mr-auto mb-3 text-center">
-      <h3>{{ __('Acá puede ir un texto.') }} </h3>
-    </div>
-    <div class="col-lg-4 col-md-6 col-sm-8 ml-auto mr-auto">
-      <form class="form" method="POST" action="{{ route('login') }}">
-        @csrf
+    <div class="container" style="height: auto;">
+        <div class="row align-items-center">
+            <div class="col-md-9 ml-auto mr-auto mb-3 text-center">
+                <h3>{{ __('Acá puede ir un texto.') }} </h3>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-8 ml-auto mr-auto">
+                <form class="form" method="POST" action="{{ route('login') }}">
+                    @csrf
 
-        <div class="card card-login card-hidden mb-3">
-          <div class="card-header card-header-primary text-center">
-            <h3 class="card-title"><strong>{{ __('Inicio de sesión') }}</strong></h3>
-          </div>
-          <div class="card-body">
-            <p class="card-description text-center">{{ __('Por favor, introduzca su nombre de usuario y contraseña.') }}  </p>
-            <div class="bmd-form-group{{ $errors->has('email') ? ' has-danger' : '' }}">
-              <div class="input-group">
-                <div class="input-group-prepend">
+                    <div class="card card-login card-hidden mb-3">
+                        <div class="card-header card-header-black text-center">
+                            <h3 class="card-title"><strong>{{ __('Inicio de sesión') }}</strong></h3>
+                        </div>
+                        <div class="card-body">
+                            <p class="card-description text-center">{{ __('Por favor, introduzca su nombre de usuario y contraseña.') }}  </p>
+                            <div class="bmd-form-group{{ $errors->has('email') ? ' has-danger' : '' }}">
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
                   <span class="input-group-text">
                     <i class="material-icons">email</i>
                   </span>
-                </div>
-                <input type="email" name="email" class="form-control" placeholder="{{ __('Email...') }}" value="{{ old('email', 'admin@material.com') }}" required>
-              </div>
-              @if ($errors->has('email'))
-                <div id="email-error" class="error text-danger pl-3" for="email" style="display: block;">
-                  <strong>{{ $errors->first('email') }}</strong>
-                </div>
-              @endif
-            </div>
-            <div class="bmd-form-group{{ $errors->has('password') ? ' has-danger' : '' }} mt-3">
-              <div class="input-group">
-                <div class="input-group-prepend">
+                                    </div>
+                                    <input type="email" name="email" class="form-control"
+                                           placeholder="{{ __('Email...') }}"
+                                           value="{{ old('email', 'admin@material.com') }}" required>
+                                </div>
+                                @if ($errors->has('email'))
+                                    <div id="email-error" class="error text-danger pl-3" for="email"
+                                         style="display: block;">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </div>
+                                @endif
+                            </div>
+                            <div class="bmd-form-group{{ $errors->has('password') ? ' has-danger' : '' }} mt-3">
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
                   <span class="input-group-text">
                     <i class="material-icons">lock_outline</i>
                   </span>
-                </div>
-                <input type="password" name="password" id="password" class="form-control" placeholder="{{ __('Password...') }}" value="{{ !$errors->has('password') ? "secret" : "" }}" required>
-              </div>
-              @if ($errors->has('password'))
-                <div id="password-error" class="error text-danger pl-3" for="password" style="display: block;">
-                  <strong>{{ $errors->first('password') }}</strong>
-                </div>
-              @endif
-            </div>
-            <div class="form-check mr-auto ml-3 mt-3">
-              <label class="form-check-label">
-                <input class="form-check-input" type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('Recuerdame') }}
-                <span class="form-check-sign">
+                                    </div>
+                                    <input type="password" name="password" id="password" class="form-control"
+                                           placeholder="{{ __('Password...') }}"
+                                           value="{{ !$errors->has('password') ? "secret" : "" }}" required>
+                                </div>
+                                @if ($errors->has('password'))
+                                    <div id="password-error" class="error text-danger pl-3" for="password"
+                                         style="display: block;">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </div>
+                                @endif
+                            </div>
+                            <div class="form-check mr-auto ml-3 mt-3">
+                                <label class="form-check-label">
+                                    <input class="form-check-input" type="checkbox"
+                                           name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('Recuerdame') }}
+                                    <span class="form-check-sign">
                   <span class="check"></span>
                 </span>
-              </label>
+                                </label>
+                            </div>
+                        </div>
+                        <div class="card-footer row ">
+                            <div class="col-6">
+                                @if (Route::has('password.request'))
+                                    <a href="{{ route('password.request') }}" class="text-dark">
+                                        <smsall>{{ __('¿Olvidó la contraseña?') }}</smsall>
+                                    </a>
+                                @endif
+                            </div>
+                            <div class="col-6 text-right">
+                                <button type="submit"
+                                        class="btn btn-primary btn-lg ">{{ __('Ingresar') }}</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+                <div class="row">
+
+                    <div class="col-12 text-right">
+                        <a href="{{ route('register') }}" class="btn btn-outline-danger btn-lg">
+                            <small>{{ __('Crear nueva cuenta') }}</small>
+                        </a>
+                    </div>
+                </div>
             </div>
-          </div>
-          <div class="card-footer justify-content-center">
-            <button type="submit" class="btn btn-primary text-light btn-lg">{{ __('Login') }}</button>
-          </div>
         </div>
-      </form>
-      <div class="row">
-        <div class="col-6">
-            @if (Route::has('password.request'))
-                <a href="{{ route('password.request') }}" class="text-dark">
-                    <small>{{ __('¿Olvidó la contraseña?') }}</small>
-                </a>
-            @endif
-        </div>
-        <div class="col-6 text-right">
-            <a href="{{ route('register') }}" class="text-dark">
-                <small>{{ __('Crear nueva cuenta') }}</small>
-            </a>
-        </div>
-      </div>
     </div>
-  </div>
-</div>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,122 +1,156 @@
 @extends('layouts.app', ['class' => 'off-canvas-sidebar', 'activePage' => 'register', 'title' => __('Material Dashboard')])
 
 @section('content')
-<div class="container" style="height: auto;">
-  <div class="row align-items-center">
-    <div class="col-lg-6 col-md-8 col-sm-12 ml-auto mr-auto">
-      <form class="form" method="POST" action="{{ route('register') }}" autocomplete="off">
-        @csrf
+    <div class="container" style="height: auto;">
+        <div class="row align-items-center">
+            <div class="col-lg-8 col-md-10 col-sm-12 ml-auto mr-auto">
+                <form class="form" method="POST" action="{{ route('register') }}" autocomplete="off">
+                    @csrf
 
-        <div class="card card-login card-hidden mb-3">
-          <div class="card-header card-header-primary text-center">
-            <h2 class="card-title"><strong>{{ __('Registro') }}</strong></h2>
-          </div>
-          <div class="card-body ">
-            <p style="font-size:12px" class="card-description text-center">{{ __('Usted necesita registrarse para poder depositar documentos en Funes. Como usuario registrado, usted podrá depositar sus documentos para que sean sometidos a una revisión editorial. Esta revisión busca únicamente verificar que los metadatos (información bibliográfica y términos clave) del documento son apropiados y comprobar que el documento es legible, completo y coherente. La revisión editorial no verifica la calidad de los documentos en ningún otro sentido. Si la revisión editorial resulta satisfactoria, su documento será colocado en el repositorio, de lo contrario le será devuelto con comentarios editoriales.') }}</p>
-            <div class="bmd-form-group{{ $errors->has('name') ? ' has-danger' : '' }}">
-              <div class="input-group">
-                <div class="input-group-prepend">
-                  <span class="input-group-text">
-                      <i class="material-icons">face</i>
-                  </span>
-                </div>
-                <input type="text" name="name" class="form-control" placeholder="{{ __('Nombre...') }}" value="{{ old('name') }}" required>
-              </div>
-              @if ($errors->has('name'))
-                <div id="name-error" class="error text-danger pl-3" for="name" style="display: block;">
-                  <strong>{{ $errors->first('name') }}</strong>
-                </div>
-              @endif
-              <div class="input-group">
-                <div class="input-group-prepend">
-                  <span class="input-group-text">
-                      <i class="material-icons">face</i>
-                  </span>
-                </div>
-                <input type="text" name="last_name" class="form-control" placeholder="{{ __('Apellido...') }}" value="{{ old('last_name') }}" required>
-              </div>
-              @if ($errors->has('last_name'))
-                <div id="last_name-error" class="error text-danger pl-3" for="last_name" style="display: block;">
-                  <strong>{{ $errors->first('last_name') }}</strong>
-                </div>
-              @endif
-            </div>
-            <div class="bmd-form-group{{ $errors->has('username') ? ' has-danger' : '' }} mt-3">
-              <div class="input-group">
-                <div class="input-group-prepend">
-                  <span class="input-group-text">
-                    <i class="material-icons">face</i>
-                  </span>
-                </div>
-                <input type="text" name="username" id="username" class="form-control" placeholder="{{ __('Nombre de usuario...') }}" value="{{ old('username') }}" autocomplete="off" required>
-              </div>
-              @if ($errors->has('username'))
-                <div id="username-error" class="error text-danger pl-3" for="username" style="display: block;">
-                  <strong>{{ $errors->first('username') }}</strong>
-                </div>
-              @endif
-            </div>
-            <div class="bmd-form-group{{ $errors->has('email') ? ' has-danger' : '' }} mt-3">
-              <div class="input-group">
-                <div class="input-group-prepend">
-                  <span class="input-group-text">
-                    <i class="material-icons">email</i>
-                  </span>
-                </div>
-                <input type="email" name="email" class="form-control" placeholder="{{ __('Correo Electrónico...') }}" value="{{ old('email') }}" required>
-              </div>
-              @if ($errors->has('email'))
-                <div id="email-error" class="error text-danger pl-3" for="email" style="display: block;">
-                  <strong>{{ $errors->first('email') }}</strong>
-                </div>
-              @endif
-            </div>
-            <div class="bmd-form-group{{ $errors->has('password') ? ' has-danger' : '' }} mt-3">
-              <div class="input-group">
-                <div class="input-group-prepend">
+                    <div class="card card-login card-hidden mb-3">
+                        <div class="card-header card-header-black text-center">
+                            <h2 class="card-title"><strong>{{ __('Registro') }}</strong></h2>
+                        </div>
+                        <div class="card-body ">
+                            <p style="font-size:12px"
+                               class="card-description text-center">{{ __('Usted necesita registrarse para poder depositar documentos en Funes. Como usuario registrado, usted podrá depositar sus documentos para que sean sometidos a una revisión editorial. Esta revisión busca únicamente verificar que los metadatos (información bibliográfica y términos clave) del documento son apropiados y comprobar que el documento es legible, completo y coherente. La revisión editorial no verifica la calidad de los documentos en ningún otro sentido. Si la revisión editorial resulta satisfactoria, su documento será colocado en el repositorio, de lo contrario le será devuelto con comentarios editoriales.') }}</p>
+                            <div class="row g-1 bmd-form-group{{ $errors->has('name') ? ' has-danger' : '' }}">
+                                <div class="col-md-6 col-sm-12">
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                          <span class="input-group-text">
+                                              <i class="material-icons">face</i>
+                                          </span>
+                                        </div>
+                                        <input type="text" name="name" class="form-control"
+                                               placeholder="{{ __('Nombre...') }}" value="{{ old('name') }}" required>
+                                    </div>
+                                    @if ($errors->has('name'))
+                                        <div id="name-error" class="error text-danger pl-3" for="name"
+                                             style="display: block;">
+                                            <strong>{{ $errors->first('name') }}</strong>
+                                        </div>
+                                    @endif
+                                </div>
+                                <div class="col-md-6 col-sm-12">
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                          <span class="input-group-text">
+                                              <i class="material-icons">face</i>
+                                          </span>
+                                        </div>
+                                        <input type="text" name="last_name" class="form-control"
+                                               placeholder="{{ __('Apellido...') }}" value="{{ old('last_name') }}"
+                                               required>
+                                    </div>
+                                    @if ($errors->has('last_name'))
+                                        <div id="last_name-error" class="error text-danger pl-3" for="last_name"
+                                             style="display: block;">
+                                            <strong>{{ $errors->first('last_name') }}</strong>
+                                        </div>
+                                    @endif
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6 col-sm-12 {{ $errors->has('username') ? ' has-danger' : '' }} mt-3">
+                                    <div class="input-group">
+                                        <div class="input-group-text">@</div>
+                                        <input type="text" name="username" id="username" class="form-control"
+                                               placeholder="{{ __('Nombre de usuario...') }}" value="{{ old('username') }}"
+                                               autocomplete="off" required>
+                                    </div>
+                                    @if ($errors->has('username'))
+                                        <div id="username-error" class="error text-danger pl-3" for="username"
+                                             style="display: block;">
+                                            <strong>{{ $errors->first('username') }}</strong>
+                                        </div>
+                                    @endif
+                                </div>
+                                <div class="col-md-6 col-sm-12 {{ $errors->has('email') ? ' has-danger' : '' }} mt-3">
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                      <span class="input-group-text">
+                                        <i class="material-icons">email</i>
+                                      </span>
+                                        </div>
+                                        <input type="email" name="email" class="form-control"
+                                               placeholder="{{ __('Correo Electrónico...') }}" value="{{ old('email') }}"
+                                               required>
+                                    </div>
+                                    @if ($errors->has('email'))
+                                        <div id="email-error" class="error text-danger pl-3" for="email"
+                                             style="display: block;">
+                                            <strong>{{ $errors->first('email') }}</strong>
+                                        </div>
+                                    @endif
+                                </div>
+                            </div>
+                            <div class="bmd-form-group{{ $errors->has('password') ? ' has-danger' : '' }} mt-3">
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
                   <span class="input-group-text">
                     <i class="material-icons">lock_outline</i>
                   </span>
-                </div>
-                <input type="password" name="password" id="password" class="form-control" placeholder="{{ __('Contraseña...') }}" required>
-              </div>
-              @if ($errors->has('password'))
-                <div id="password-error" class="error text-danger pl-3" for="password" style="display: block;">
-                  <strong>{{ $errors->first('password') }}</strong>
-                </div>
-              @endif
-            </div>
-            <div class="bmd-form-group{{ $errors->has('password_confirmation') ? ' has-danger' : '' }} mt-3">
-              <div class="input-group">
-                <div class="input-group-prepend">
+                                    </div>
+                                    <input type="password" name="password" id="password" class="form-control"
+                                           placeholder="{{ __('Contraseña...') }}" required>
+                                </div>
+                                @if ($errors->has('password'))
+                                    <div id="password-error" class="error text-danger pl-3" for="password"
+                                         style="display: block;">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </div>
+                                @endif
+                            </div>
+                            <div
+                                class="bmd-form-group{{ $errors->has('password_confirmation') ? ' has-danger' : '' }} mt-3">
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
                   <span class="input-group-text">
                     <i class="material-icons">lock_outline</i>
                   </span>
-                </div>
-                <input type="password" name="password_confirmation" id="password_confirmation" class="form-control" placeholder="{{ __('Confirmar Contraseña...') }}" required>
-              </div>
-              @if ($errors->has('password_confirmation'))
-                <div id="password_confirmation-error" class="error text-danger pl-3" for="password_confirmation" style="display: block;">
-                  <strong>{{ $errors->first('password_confirmation') }}</strong>
-                </div>
-              @endif
-            </div>
-            <div class="form-check mr-auto ml-3 mt-3">
-              <label class="form-check-label">
-                <input class="form-check-input" type="checkbox" id="policy" name="policy" {{ old('policy', 1) ? 'checked' : '' }} >
-                <span class="form-check-sign">
+                                    </div>
+                                    <input type="password" name="password_confirmation" id="password_confirmation"
+                                           class="form-control" placeholder="{{ __('Confirmar Contraseña...') }}"
+                                           required>
+                                </div>
+                                @if ($errors->has('password_confirmation'))
+                                    <div id="password_confirmation-error" class="error text-danger pl-3"
+                                         for="password_confirmation" style="display: block;">
+                                        <strong>{{ $errors->first('password_confirmation') }}</strong>
+                                    </div>
+                                @endif
+                            </div>
+                            <div class="form-check mr-auto ml-3 mt-3">
+                                <label class="form-check-label">
+                                    <input class="form-check-input" type="checkbox" id="policy"
+                                           name="policy" {{ old('policy', 1) ? 'checked' : '' }} >
+                                    <span class="form-check-sign">
                   <span class="check"></span>
                 </span>
-                {{ __('Estoy de acuerdo con la ') }} <a href="#">{{ __('Política de privacidad') }}</a>
-              </label>
+                                    {{ __('Estoy de acuerdo con la ') }} <a
+                                        href="#">{{ __('Política de privacidad') }}</a>
+                                </label>
+                            </div>
+                        </div>
+                        <div class="card-footer justify-content-center">
+
+                        </div>
+
+                        <div class="card-footer row ">
+                            <div class="col-6">
+                                <a href="{{ route('login') }}" class="text-dark">
+                                    <smsall>{{ __('¿Ya tiene una cuenta?') }}</smsall>
+                                </a>
+                            </div>
+                            <div class="col-6 text-right">
+                                <button type="submit"
+                                        class="btn btn-primary btn-lg">{{ __('Crear cuenta') }}</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
             </div>
-          </div>
-          <div class="card-footer justify-content-center">
-            <button type="submit" class="btn btn-primary btn-lg">{{ __('Crear cuenta') }}</button>
-          </div>
         </div>
-      </form>
     </div>
-  </div>
-</div>
 @endsection
