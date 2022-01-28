6da500fd9a53c1210b9ae55b31d671c86b34da7f
Leandro Moreno <123857@gmail.com>
2022-01-27 22:20:12 -0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\SubjectController;
+use App\Http\Controllers\Api\CountApiController;
 use App\Http\Controllers\RegistroController;
 use App\Http\Controllers\AutorInstitucionalController;
 use App\Http\Controllers\Api\Search\RegistroApiController;
@@ -11,6 +12,8 @@ Route::get('/user', [AuthorController::class, 'index']);
 Route::get('/buscar-autor-institucional', [AutorInstitucionalController::class, 'index']);
 Route::get('/subject/{subject}', [SubjectController::class, 'showApi'])->name('api.subject.show');
 Route::get('/search/registro', [RegistroApiController::class, 'search'])->name('api.search.registro');
+Route::get('/count', [CountApiController::class, 'registros'])->name('api.count.registro');
+
 //Route::get('/tipos-registro', RegistroController::class, 'tiposRegistro');
 //Route::get('/campos-tipos-registro', RegistroController::class, 'camposTiposRegistro');
 //Route::get('/registro', [RegistroApiController::class, 'index'])->name('registro.index.api');
