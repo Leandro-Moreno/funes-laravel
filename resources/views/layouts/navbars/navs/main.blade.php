84b0ddb20aa0ea357969350e4bac219bfafc59cf
Leandro Moreno <123857@gmail.com>
2022-02-21 22:08:58 -0500

    Update main.blade.php

--- resources/views/layouts/navbars/navs/main.blade.php
+++ resources/views/layouts/navbars/navs/main.blade.php
@@ -1,43 +1,44 @@
-<nav class="navbar navbar-expand-lg bg-primary-light navbar-absolute text-dark">
-    <div class="container">
-        <div class="navbar-wrapper">
-            <a style="font-size:52px;" class="navbar-brand andes-funes" href="{{ route('home') }}"></a>
-        </div>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index"
-                aria-expanded="false" aria-label="Toggle navigation">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="navbar-toggler-icon icon-bar"></span>
-            <span class="navbar-toggler-icon icon-bar"></span>
-            <span class="navbar-toggler-icon icon-bar"></span>
-        </button>
-        <div class="collapse navbar-collapse justify-content-end">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a href="{{ route('registro.index') }}" class="nav-link">
-                        <i class="material-icons">article</i> {{ __('Registros') }}
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="{{ route('subject.index') }}" class="nav-link">
-                        <i class="material-icons">dashboard</i> {{ __('Término Clave') }}
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="{{ route('author.index') }}" class="nav-link">
-                        <i class="material-icons">dashboard</i> {{ __('Autor') }}
-                    </a>
-                </li>
-                <li class="nav-item{{ $activePage == 'register' ? ' active' : '' }}">
-                    <a href="{{ route('editorial') }}" class="nav-link">
-                        <i class="material-icons">person_add</i> {{ __('Editorial') }}
-                    </a>
-                </li>
-                <li class="nav-item{{ $activePage == 'login' ? ' active' : '' }}">
-                    <a href="{{ route('year') }}" class="nav-link">
-                        <i class="material-icons">fingerprint</i> {{ __('Año') }}
-                    </a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
+{{--<nav class="navbar navbar-expand-lg bg-primary-light navbar-absolute text-dark">--}}
+{{--    <div class="container">--}}
+{{--        <div class="navbar-wrapper">--}}
+{{--            <a style="font-size:52px;" class="navbar-brand andes-funes" href="{{ route('home') }}"></a>--}}
+{{--        </div>--}}
+{{--        <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index"--}}
+{{--                aria-expanded="false" aria-label="Toggle navigation">--}}
+{{--            <span class="sr-only">Toggle navigation</span>--}}
+{{--            <span class="navbar-toggler-icon icon-bar"></span>--}}
+{{--            <span class="navbar-toggler-icon icon-bar"></span>--}}
+{{--            <span class="navbar-toggler-icon icon-bar"></span>--}}
+{{--        </button>--}}
+{{--        <div class="collapse navbar-collapse justify-content-end">--}}
+{{--            <ul class="navbar-nav">--}}
+{{--                <li class="nav-item">--}}
+{{--                    <a href="{{ route('registro.index') }}" class="nav-link">--}}
+{{--                        <i class="material-icons">article</i> {{ __('Registros') }}--}}
+{{--                    </a>--}}
+{{--                </li>--}}
+{{--                <li class="nav-item">--}}
+{{--                    <a href="{{ route('subject.index') }}" class="nav-link">--}}
+{{--                        <i class="material-icons">dashboard</i> {{ __('Término Clave') }}--}}
+{{--                    </a>--}}
+{{--                </li>--}}
+{{--                <li class="nav-item">--}}
+{{--                    <a href="{{ route('author.index') }}" class="nav-link">--}}
+{{--                        <i class="material-icons">dashboard</i> {{ __('Autor') }}--}}
+{{--                    </a>--}}
+{{--                </li>--}}
+{{--                <li class="nav-item{{ $activePage == 'register' ? ' active' : '' }}">--}}
+{{--                    <a href="{{ route('editorial') }}" class="nav-link">--}}
+{{--                        <i class="material-icons">person_add</i> {{ __('Editorial') }}--}}
+{{--                    </a>--}}
+{{--                </li>--}}
+{{--                <li class="nav-item{{ $activePage == 'login' ? ' active' : '' }}">--}}
+{{--                    <a href="{{ route('year') }}" class="nav-link">--}}
+{{--                        <i class="material-icons">fingerprint</i> {{ __('Año') }}--}}
+{{--                    </a>--}}
+{{--                </li>--}}
+{{--            </ul>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+{{--</nav>--}}
+<menu-lateral></menu-lateral>
