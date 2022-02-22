e5efb7df4078cce95ac69bcb2e54eebf2cbaa994
Leandro Moreno <123857@gmail.com>
2022-02-21 22:08:56 -0500

    Update guest.blade.php

--- resources/views/layouts/navbars/navs/guest.blade.php
+++ resources/views/layouts/navbars/navs/guest.blade.php
@@ -1,13 +1,13 @@
 <!-- Navbar -->
 <div class="fixed-top">
-   <section class="d-none d-md-block p-1 text-white banner-top bg-black">
-      <div class="container container-fluid menu-adm">
-         <ul class="ep_tm_key_tools" id="ep_tm_menu_tools">
-            <li><a href="{{ route('register') }}">Registro</a></li>
-            <li><a href="{{ route('login') }}">Iniciar Sesión</a></li>
-         </ul>
-      </div>
-   </section>
+{{--   <section class="d-none d-md-block p-1 text-white banner-top bg-black">--}}
+{{--      <div class="container container-fluid menu-adm">--}}
+{{--         <ul class="ep_tm_key_tools" id="ep_tm_menu_tools">--}}
+{{--            <li><a href="{{ route('register') }}">Registro</a></li>--}}
+{{--            <li><a href="{{ route('login') }}">Iniciar Sesión</a></li>--}}
+{{--         </ul>--}}
+{{--      </div>--}}
+{{--   </section>--}}
     @include('layouts.navbars.navs.main')
    <!-- End Navbar -->
 </div>
