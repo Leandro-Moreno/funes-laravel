6cc57188542716c0afcc68416b234d2a67dbd865
Leandro Moreno <123857@gmail.com>
2022-02-21 22:09:01 -0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\RoutesController;
 use App\Http\Controllers\SubjectController;
 use App\Http\Controllers\Api\CountApiController;
 use App\Http\Controllers\RegistroController;
@@ -14,6 +15,7 @@ Route::get('/subject/{subject}', [SubjectController::class, 'showApi'])->name('a
 Route::get('/search/registro', [RegistroApiController::class, 'search'])->name('api.search.registro');
 Route::get('/count', [CountApiController::class, 'registros'])->name('api.count.registro');
 Route::get('/latest', [RegistroApiController::class, 'latest'])->name('api.registro.latest');
+Route::get('/routes', [RoutesController::class, 'show'])->name('api.routes.show');
 
 //Route::get('/tipos-registro', RegistroController::class, 'tiposRegistro');
 //Route::get('/campos-tipos-registro', RegistroController::class, 'camposTiposRegistro');
