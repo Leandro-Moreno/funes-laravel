195e7dec0f1ab8535515fe0946d7f33924480ea7
Leandro Moreno <123857@gmail.com>
2022-02-22 18:14:06 -0500

    Update show.blade.php

--- resources/views/registros/show.blade.php
+++ resources/views/registros/show.blade.php
@@ -1,4 +1,9 @@
-@extends('layouts.app', ['class' => 'off-canvas-sidebar', 'activePage' => 'registro', 'title' => __('Funes - ').$registro->title])
+@extends('layouts.app-registro', [
+    'class' => 'off-canvas-sidebar',
+     'activePage' => 'registro',
+      'title' => __('Funes - ').$registro->title,
+      'canonical' => route('registro.show', $registro),
+      ])
 
 @section('content')
 <registro-index :registro='@json( $registro )'></registro-index>
