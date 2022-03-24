32667fea424ec847a757a9ae48cacb78ef555e4a
Leandro Moreno <123857@gmail.com>
2022-03-24 15:52:57 -0500

    Update index.blade.php

--- resources/views/authors/index.blade.php
+++ resources/views/authors/index.blade.php
@@ -10,17 +10,10 @@
                 <a href="{{route('author.show', $author)}}" class="col-md-3">
                     <div class="card">
                         <div class="card-header">
-                            <h2 class="card-title">
-                            </h2>
-                            <div class="card-description">
-                                <h3 class="card-title">{{$author->given}} {{$author->family}}</h3>
-                                <p>{{$author->email}}</p>
-                            </div>
+                            <p class="badge badge-black">{{$author->registros_count}} {{__('registros')}}</p>
                         </div>
                         <div class="card-body">
-                            <div class="row">
-                                <p>{{$author->registros_count}} {{__('registros')}}</p>
-                            </div>
+                            <h3 class="car">{{$author->family}}, {{$author->given}}</h3>
                         </div>
                     </div>
                 </a>
