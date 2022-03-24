3c802a9bc6699ce8838f55b07e1584ffd4b27cb5
Leandro Moreno <123857@gmail.com>
2022-03-24 15:49:10 -0500

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Subject;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Collection;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    public function subjectIdsRecursiveChildren(Subject $subject)
+    {
+        $ids = new Collection();
+        $ids->push($subject->id);
+        if($subject->children->count()>0)
+        {
+            foreach( $subject->children as $children){
+                $ids->push($children->id);
+                $ids = $ids->merge($this->subjectIdsRecursiveChildren($children));
+            }
+        }
+        return $ids;
+    }
 }
